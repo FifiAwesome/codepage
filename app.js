5cd5722bf01df6737a8f7cc104becb49411b3d0c
FifiAwesome <filip.urbanski@gmail.com>
2017-02-21 22:15:45 +0100

    chuj strzelil

--- app.js
+++ app.js
@@ -7,11 +7,63 @@ var app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 mongoose.connect("mongodb://localhost:27017/codepage");
 
+// models
+var UnitSchema = new mongoose.Schema ({
+  name: String,
+  url: String,
+  links: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Link"
+    }
+  ]
+});
+var Unit = mongoose.model("Unit", UnitSchema);
+
+var LinkSchema = new mongoose.Schema ({
+  name: String,
+  url: String
+});
+var Link = mongoose.model("Link", LinkSchema);
+
+
+
+// routes
 app.get("/", function(req, res){
-  res.render("index");
+  Unit.find({}, function(err, units){
+    if(err){
+      console.log(err);
+    } else {
+      Link.find({}, function(err, links){
+        if(err){
+          console.log("Error --> ");
+        } else {
+          res.render("index", {units: units, links:links});
+        }
+      })
+    }
+  })
+});
+
+//units
+app.get("/new", function(req, res){
+  res.render("units/new");
+});
+
+app.post("/new", function(req, res){
+  Unit.create(req.body.unit, function(err, createdUnit){
+    if(err){
+      console.log(err);
+    } else {
+      console.log(createdUnit);
+      return res.redirect("/");
+    }
+  })
 });
 
 app.listen(process.env.PORT, function(){
---
+++ public/scripts/main.js
@@ -0,0 +1,5 @@
+// $(".plus.icon:first").on("click", function(){
+//   $(".ui.form:first").fadeToggle();
+//   //rolls a form to add new icons
+//   //adds new icon
+// })
