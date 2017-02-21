903fce377af541ec34729c8dac901c48732c8d68
FifiAwesome <filip.urbanski@gmail.com>
2017-02-21 14:15:43 +0100

    body-parser and method-override setup

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require("express"),
+    bodyParser = require("body-parser"),
+    methodOverride = require("method-override"),
     mongoose = require("mongoose");
 
 var app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
 mongoose.connect("mongodb://localhost:27017/codepage");
 
-
+app.get("/", funtion(req, res){
+  res.render("index");
+});
 
 app.listen(process.env.PORT, function(){
   console.log("CodePage server started.")
