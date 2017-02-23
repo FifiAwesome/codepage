9bd99427ffdd82df1cd117f223ca50053266803e
FifiAwesome <filip.urbanski@gmail.com>
2017-02-23 12:17:09 +0100

    set up database

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 
-mongoose.connect("mongodb://localhost:27017/codepage");
+mongoose.connect(process.env.DATABASE);
 
 // models
 var UnitSchema = new mongoose.Schema ({
